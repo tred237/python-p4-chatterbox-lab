8ed904b13e6724047bb5773f0bb66e3a44c4ae0c
trevor dewindt <trevor.dewindt@gmail.com>
2023-12-18 20:08:32 -0700

    add final code

--- server/app.py
+++ server/app.py
@@ -14,13 +14,45 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+import ipdb
 
-@app.route('/messages/<int:id>')
+@app.route('/messages', methods=['GET', 'POST'])
+def messages():
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.all():
+            messages.append(message.to_dict())
+        return make_response(
+            messages,
+            200
+        )
+    elif request.method == 'POST':
+        message = Message(
+            username = request.get_json().get('username'),
+            body = request.get_json().get('body')
+        )
+        db.session.add(message)
+        db.session.commit()
+        return make_response(
+            message.to_dict(),
+            201
+        )
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    if request.method == 'PATCH':
+        for attribute in request.get_json():
+            setattr(message, attribute, request.get_json().get(attribute))
+        db.session.add(message)
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+
+    db.session.commit()
+    return make_response(
+        message.to_dict(),
+        200
+    )
 
 if __name__ == '__main__':
     app.run(port=5555)
